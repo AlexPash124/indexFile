018ed9f72ff56a1ea298b81f7c808bb395aed1ac
Ваше имя <you@example.com>
2022-01-12 16:45:57 +0200

    Init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let arr=['pl.png', 'orange', '3', 66];
+//alert(arr[3]);
+//const result=confirm("you still here?");
+//alert(result);
+
+// const answer=+prompt("Вам є 18 років?", "18");
+// alert(answer+10);
+const answers=[];
+answers[0]=prompt('You name?','');
+answers[1]=prompt('You family?','');
+answers[3]=prompt('You years?','');
+
+console.log(typeof(answers));
